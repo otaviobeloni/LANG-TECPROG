68d29a20d9290095ce90f80e4a1e9106a3f00804
otaviobeloni <otaviobeloni.esoft@gmail.com>
2026-08-19 11:29:25 -0300

    Update and rename Lista1 (entrada e saida).c to LISTA 01 (atividade: entrada e saida).c

--- LISTA 01 (atividade: entrada e saida).c
+++ LISTA 01 (atividade: entrada e saida).c
@@ -10,7 +10,9 @@ int main(int argc, char *argv[]) {
 	//Primeiro Exercicio
 	int primeiro, segundo, aux;
 	
-	printf("Escreva o primeiro numero: ");
+	printf("PRIMEIRO EXERCICIO");
+	
+	printf("\nEscreva o primeiro numero: ");
 	scanf("%d", &primeiro);
 	printf("Escreva o segundo numero: ");
 	scanf("%d", &segundo);
@@ -25,7 +27,9 @@ int main(int argc, char *argv[]) {
 	double valor, A;
 	int N;
 	
-	printf("\nInforme um valor do tipo double: ");
+	printf("\n\nSEGUNDO EXERCICIO");
+	
+	printf("\nInforme um valor do tipo double para mostrar em notacao cientifica: ");
 	scanf("%lf", &valor);
 	
 	N = (int) log10(valor);
@@ -34,9 +38,44 @@ int main(int argc, char *argv[]) {
 	printf("%f x 10^%d", A, N);
 	
 	
+	//Terceiro exercicio
+	int n, resultado, bit64, bit32, bit16, bit8, bit4, bit2;
+	
+	printf("\n\nTERCEIRO EXERCICIO");
+	
+	printf("\nInforme um valor para transformar em binario (ate 127): ");
+	scanf("%d", &n);
+	
+	bit64 = n%2;
+	resultado = n/2;
+	
+	bit32 = resultado%2;
+	resultado = resultado/2;
+	
+	bit16 = resultado%2;
+	resultado = resultado/2;
+	
+	bit8 = resultado%2;
+	resultado = resultado/2;
+	
+	bit4 = resultado%2;
+	resultado = resultado/2;
+
+	bit2 = resultado%2;
+	resultado = resultado/2;
+	
+	
+	printf("O numero %d em binario e igual a : %d%d%d%d%d%d%d", n, resultado%2, bit2, bit4, bit8, bit16, bit32, bit64);
+
+	
+	
+	
+	
 	//Quarto exercicio
 	int salario;
 	float comissao, total;
+	
+	printf("\n\nQUARTO EXERCICIO");
 	
 	printf("\nQual e o seu salario?: ");
 	scanf("%d", &salario);	
@@ -51,6 +90,8 @@ int main(int argc, char *argv[]) {
 	//Quinto exercicio
 	float n1, n2, n3, n4;
 	
+	printf("\n\nQUINTO EXERCICIO / (SOMA MEDIA E PRODUTORIO)");
+	
 	printf("\nEscreva o primeiro numero: ");
 	scanf("%f", &n1);
 	printf("Escreva o segundo numero: ");
@@ -60,12 +101,14 @@ int main(int argc, char *argv[]) {
 	printf("Escreva o quarto numero: ");
 	scanf("%f", &n4);
 	
-	printf("A soma entre %f, %f, %f e %f e igual a: %f ", n1, n2, n3, n4, n1+n2+n3+n4);
-	printf("\nA média entre %f, %f, %f e %f e igual a: %f ", n1, n2, n3, n4, (n1+n2+n3+n4)/4);
+	printf("A soma entre %.2f, %.2f, %.2f e %.2f e igual a: %.2f ", n1, n2, n3, n4, n1+n2+n3+n4);
+	printf("\nA media entre %.2f, %.2f, %.2f e %.2f e igual a: %.2f ", n1, n2, n3, n4, (n1+n2+n3+n4)/4);
 	printf("\nO produtorio entre %.2f, %.2f, %.2f e %.2f e igual a: %.2f ", n1, n2, n3, n4, n1*n2*n3*n4);
 	
 	//Sexto exercicio
 	int diasidade, anos, meses, dias;
+	
+	printf("\n\nSEXTO EXERCICIO");
 	
 	printf("\nquantos dias de vida voce tem?: ");
 	scanf("%d", &diasidade);
@@ -75,11 +118,13 @@ int main(int argc, char *argv[]) {
 	meses = diasidade/30;
 	dias = diasidade%30;
 	
-	printf("Então você tem %d anos, %d meses e %d dias de vida!", anos, meses, dias);
+	printf("Entao voce tem %d anos, %d meses e %d dias de vida!", anos, meses, dias);
 		
 	
 	//Setimo exercicio
 	int raio;
+	
+	printf("\n\nSETIMO EXERCICIO");
 	
 	printf("\nInforme o valor do raio da esfera: ");
 	scanf("%d", &raio);
@@ -91,16 +136,20 @@ int main(int argc, char *argv[]) {
 	//Oitavo exercicio
 	int X1, X2, Y1, Y2;
 	
+	printf("\n\nOITAVO EXERCICIO");
+	
 	printf("\nInforme o valor de X1: ");
 	scanf("%d", &X1);
-	printf("\nInforme o valor de X2: ");
-	scanf("%d", &X2);
 	printf("\nInforme o valor de Y1: ");
 	scanf("%d", &Y1);
+	printf("\nInforme o valor de X2: ");
+	scanf("%d", &X2);
 	printf("\nInforme o valor de Y2: ");
 	scanf("%d", &Y2);
 	
 	float coordenada = sqrt(pow(X2 - X1, 2) + pow(Y2 - Y1, 2));
+	
+	printf("A coordenada e igual a: %f", coordenada);
 	
 	return 0;
 }
